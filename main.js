8ba6487c7f4fcd900b3ad2e0b21528661c0d2f18
misbahsiddiqq <misbah.dhanani@gmail.com>
2024-03-31 21:33:59 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,43 @@
+import inquirer from "inquirer";
+let myBalance = 10000;
+let myPin = 9999;
+let pinAnswer = await inquirer.prompt([
+    {
+        type: "number",
+        name: "Pin",
+        message: "Enter your pin?"
+    }
+]);
+if (pinAnswer.Pin === myPin) {
+    console.log("Correct pin code");
+}
+else {
+    console.log("Incorrect pin code");
+}
+;
+let operaionAns = await inquirer.prompt([{
+        type: "list",
+        name: "operation",
+        message: "Please select option",
+        choices: ["withdraw", "Check balance"]
+    }
+]);
+console.log(operaionAns);
+if (operaionAns.operation === "withdraw") {
+    let amountAns = await inquirer.prompt([
+        {
+            type: "list",
+            choices: ["1000", "5000", "7000", "10000", "Enter amount to draw"],
+            name: "amount",
+            message: "Enter amount to withdraw"
+        }
+    ]);
+    myBalance -= amountAns.amount;
+    console.log("Your remaining balance: " + myBalance);
+}
+else if (operaionAns.operation === "Check balance") {
+    console.log("Your balance is " + myBalance);
+}
+// else{
+//     console.log("Incorrect pin number")
+// } 
